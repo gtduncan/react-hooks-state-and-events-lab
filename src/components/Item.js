ab4c6768b1e8643796acbfa509d156745d31c468
George Duncan <gtduncan@usc.edu>
2022-07-21 14:58:31 -0400

    commit tree 8abbe35d76cc7d725efc4dae2fd4e7ace46d4aab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,28 @@
 import React from "react";
+import {useState} from "react";
 
 function Item({ name, category }) {
+  
+  const [inCart, setInCart] = useState('false')
+
+  const toggleCart = () =>
+  {
+    setInCart((inCart) => !inCart)
+  }
+  
+  const cartClass = inCart ? 'in-cart' : 'add'
+
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
+    <li className={inCart ? ''  : "in-cart"}>
+    <span>{name}</span>
+    <span className="category">{category}</span>
+    <button
+      className={inCart ? "add" : "remove"}
+      onClick={toggleCart}
+    >
+      {inCart ? "Add to" : "Remove from"} Cart
+    </button>
+  </li>
   );
 }
 
